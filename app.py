b6b87d98511166fa4b0e1509f4a095c510399d0c
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-18 00:49:52 -0600

    graficas dinamicas semicompleto

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask,redirect,url_for,render_template,request
 import operaciones as op
 app=Flask(__name__)
 
+
 @app.route('/',methods=['GET','POST'])
 def home():
     escuelas=op.datos_esc()
     return render_template('index.html',escuelas=escuelas)
+
+@app.route("/obtener_graficas",methods=['GET','POST'])
+def obtener_graficas():
+    if request.method=="POST":
+        grafica = request.form.get('graficame')
+        escuelas=op.datos_esc()
+        return render_template('index.html',escuelas=escuelas,defecto=grafica)
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
--- operaciones.py
+++ operaciones.py
@@ -11,3 +11,14 @@ def datos_esc():
         escuelas_unicas.append(datos_esc.index.values[i])
     
     return escuelas_unicas
+
+def graficame(escuela):
+    selector=data[(data.Escuela==escuela)]
+    """
+    Aquí pones el codigo para hacer las graficas, haz la pruebas en el archivo jupyter y solo copia y pega
+    el codigo aquí, exportas la ruta estatica de las imagenes las metes en un arreglo, las envias aqui con 
+    el return, las envias en la template con el nombre que aparece en el index 'img_gra' y agregas la ruta
+    estatica al src y listo solo le das estilo
+    """
+
+    print(selector)
